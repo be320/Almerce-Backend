37042e91d4ff631b91b5a0cb1bc21cf259ef34fc
andrewkuzman <andrewr.kuzman@gmail.com>
2021-01-23 20:07:56 +0200

    not my job

--- app/main.py
+++ app/main.py
@@ -25,23 +25,24 @@ def sendText():
 @app.route('/track', methods=["POST"])
 def track():
     request_data = request.get_json()
-    event_type = request_data["event"]
-    if event_type == "Product Added":
-        product_event_details(request_data)
-    #elif event_type == "Product Clicked":
+    if request_data.has_key("event"):
+        event_type = request_data["event"]
+        if event_type == "Product Added":
+            product_event_details(request_data)
+        #elif event_type == "Product Clicked":
 
-    #elif event_type == "Product Removed":
+        #elif event_type == "Product Removed":
 
-    #elif event_type == "Order Completed":
-    
-    #else:
-    #    print("Other events not handled")
+        #elif event_type == "Order Completed":
+        
+        #else:
+        #    print("Other events not handled")
 
-#    print(request_data)
-    data = {}
-    data["reply"] = "Data Received"
-    data["status"] = 'success'
-    return jsonify(data)
+    #    print(request_data)
+        data = {}
+        data["reply"] = "Data Received"
+        data["status"] = 'success'
+        return jsonify(data)
 
 
 # A welcome message to test our server
