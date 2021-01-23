1bf822816d400d3c72d3a5232d3e2026b6aa03e6
Ahmed Bahaa <ahmedbahaa.3@hotmail.com>
2021-01-23 17:31:43 +0200

    First Commit

---
+++ app/main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+# Text Messages
+@app.route('/sendText', methods=["POST"])
+@cross_origin()
+def sendText():
+    request_data = request.get_json()
+    print(request_data)
+    request_data["Template"]["serverSide"] = True
+    data = {}
+    data["reply"] = "Data Received"
+    data["status"] = 'success'
+    return jsonify(request_data)
+
+
+# Track Click Events
+@app.route('/track', methods=["POST"])
+def track():
+    if request.method == "POST":
+        request_data = request.get_json()
+        print(request_data)
+        data = {}
+        data["reply"] = "Data Received"
+        data["status"] = 'success'
+        return jsonify(data)
+
+
+# A welcome message to test our server
+@app.route('/')
+@cross_origin()
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+
+
+
+
+
