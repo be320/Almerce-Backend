ed7e9413ea190e69bc694ccee505c91e2e122b67
andrewkuzman <andrewr.kuzman@gmail.com>
2021-01-24 16:50:25 +0200

    .replace("\xa0", "").split(" ,")

--- app/order_completed.py
+++ app/order_completed.py
@@ -1,7 +1,7 @@
 def order_completed_details(data):
     details = {
     "event": "Order Completed",
-    "products_names": data["properties"]["Order Details"].split(" ,").replace("\xa0", ""),
+    "products_names": data["properties"]["Order Details"].replace("\xa0", "").split(" ,"),
     "session_id": data["anonymousId"],
     "user_id": data["userId"],
     "timestamp": data["timestamp"]    
